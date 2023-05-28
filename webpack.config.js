3ec7e4b07252a490006e9d5b790d9f15ff5bdbbd
Matulg47 <matu47@hotmail.com>
2023-05-28 12:36:38 -0300

    28/5

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/javascript/packs/application.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      // Otros loaders y reglas necesarios para tu proyecto
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+    extensions: ['*', '.js', '.vue', '.json'],
+  },
+};
